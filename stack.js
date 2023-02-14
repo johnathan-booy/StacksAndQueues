3aa9b04573b5acc6fdb7394b20b0e5938f7b9330
Johnathan Booy <106690990+johnathan-booy@users.noreply.github.com>
2023-02-14 07:44:01 -0800

    Stack implementation

--- stack.js
+++ stack.js
@@ -1,46 +1,73 @@
 /** Node: node for a stack. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
 }
 
 /** Stack: chained-together nodes where you can
  *  remove from the top or add to the top. */
 
 class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
+	/** push(val): add new value to end of the stack. Returns undefined. */
 
-  push(val) {
+	push(val) {
+		const newNode = new Node(val);
 
-  }
+		if (this.first) {
+			newNode.next = this.first;
+		} else {
+			this.last = newNode;
+		}
 
-  /** pop(): remove the node from the top of the stack
-   * and return its value. Should throw an error if the stack is empty. */
+		this.first = newNode;
 
-  pop() {
+		this.size++;
+	}
 
-  }
+	/** pop(): remove the node from the top of the stack
+	 * and return its value. Should throw an error if the stack is empty. */
 
-  /** peek(): return the value of the first node in the stack. */
+	pop() {
+		// Throws an error if the stack is empty
+		if (!this.size) throw new Error("Stack is empty");
 
-  peek() {
+		const node = this.first;
 
-  }
+		this.first = this.first.next;
 
-  /** isEmpty(): return true if the stack is empty, otherwise false */
+		this.size--;
 
-  isEmpty() {
+		if (this.size === 0) this.last = null;
 
-  }
+		return node.val;
+	}
+
+	/** peek(): return the value of the first node in the stack. */
+
+	peek() {
+		if (this.first) {
+			return this.first.val;
+		}
+	}
+
+	/** isEmpty(): return true if the stack is empty, otherwise false */
+
+	isEmpty() {
+		if (this.size > 0) {
+			return false;
+		}
+
+		return true;
+	}
 }
 
 module.exports = Stack;
